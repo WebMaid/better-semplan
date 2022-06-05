c11a7a620c236253f80cb009df21af79a7c04858
Meierschlumpf <meierschlumpf@gmail.com>
2022-06-05 09:54:39 +0200

    feat: add teacher list view #BETSEM-24

--- apps/web/src/app/pages/teacher/list/component.tsx
+++ apps/web/src/app/pages/teacher/list/component.tsx
@@ -1,7 +1,47 @@
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Avatar, Group, Table, TextInput } from "@mantine/core";
+import { useTranslation } from "react-i18next";
 import { IndexProps } from ".";
 
-interface Props extends IndexProps {}
+interface Props extends IndexProps {
+	teachers: any[];
+}
 
-export const TeacherListPageComponent = ({}: Props) => {
-	return <div>Teachers</div>;
+export const TeacherListPageComponent = ({ teachers }: Props) => {
+	const { t } = useTranslation();
+
+	return (
+		<>
+			<h1>{t("teacher.list.title")}</h1>
+			<Group>
+				<TextInput
+					icon={<FontAwesomeIcon icon={faSearch} />}
+					placeholder={t("search.title")}
+				/>
+			</Group>
+			<Table striped highlightOnHover>
+				<thead>
+					<tr>
+						<th style={{ width: "50px" }}></th>
+						<th>{t("teacher.properties.fullName")}</th>
+						<th>{t("teacher.properties.hireDate.label")}</th>
+						<th>{t("teacher.properties.workLoad.label")}</th>
+					</tr>
+				</thead>
+				<tbody>
+					{teachers.map((teacher) => (
+						<tr key={teacher.id}>
+							<td>
+								<Avatar />
+							</td>
+							<td>{`${teacher.firstname} ${teacher.lastname}`}</td>
+							<td>{teacher.hireDate}</td>
+							<td>{teacher.workLoad}%</td>
+						</tr>
+					))}
+				</tbody>
+			</Table>
+		</>
+	);
 };
--- apps/web/src/app/pages/teacher/list/index.tsx
+++ apps/web/src/app/pages/teacher/list/index.tsx
@@ -3,5 +3,14 @@ import { TeacherListPageComponent } from "./component";
 export interface IndexProps {}
 
 export const TeacherListPage = (indexProps: IndexProps) => {
-	return <TeacherListPageComponent {...indexProps} />;
+	const teachers: any[] = [
+		{
+			id: "1",
+			firstname: "Nadja",
+			lastname: "Burri",
+			workLoad: 50,
+			hireDate: "2015-03-02",
+		},
+	];
+	return <TeacherListPageComponent {...indexProps} teachers={teachers} />;
 };
